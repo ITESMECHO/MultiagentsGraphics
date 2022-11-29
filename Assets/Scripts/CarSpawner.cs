c34e4c1f979b9d885e4c1b97c977107bfa047b21
CristianCazares <cm_cjavier@hotmail.com>
2022-11-28 18:53:14 -0600

    feat(CarSpawner): Adjusted direction in-game to direction in JSON

--- Assets/Scripts/CarSpawner.cs
+++ Assets/Scripts/CarSpawner.cs
@@ -15,8 +15,54 @@ public class CarSpawner : MonoBehaviour
         _carsExecution = new GameObject[cars.Length];
         for (int i = 0; i < cars.Length; i++)
         {
-            Car currentCar = cars[i];
-            if (i < 10)
+            int[] currentDirection = cars[i].direction;
+            int yRotation = 0;
+            string carName = "";
+
+
+            /*
+             * [0,  1] = 0
+             * [0, -1] = 180
+             * [1,  0] = 90
+             * [-1, 0] = -90
+             */
+            if (currentDirection[0] == 0 && currentDirection[1] == 1)
+            {
+                _carsExecution[i] = CarpoolManager.Instance.ActivateObject(
+                        new Vector3(cars[i].x - _offset + _offsetStreet, cars[i].y, cars[i].z / _ratio)
+                );
+                yRotation = 0;
+                carName = "S Car ";
+            }
+            else if (currentDirection[0] == 0 && currentDirection[1] == -1)
+            {
+                _carsExecution[i] = CarpoolManager.Instance.ActivateObject(
+                        new Vector3(cars[i].x + _offset - _offsetStreet, cars[i].y, cars[i].z / _ratio)
+                );
+                yRotation = 180;
+                carName = "N Car ";
+            }
+            else if (currentDirection[0] == 1 && currentDirection[1] == 0)
+            {
+                _carsExecution[i] = CarpoolManager.Instance.ActivateObject(
+                        new Vector3(cars[i].x / _ratio, cars[i].y, cars[i].z)
+                );
+                yRotation = -90;
+                carName = "W Car ";
+            }
+            else if (currentDirection[0] == -1 && currentDirection[1] == 0)
+            {
+                _carsExecution[i] = CarpoolManager.Instance.ActivateObject(
+                        new Vector3(cars[i].x / _ratio, cars[i].y, cars[i].z)
+                );
+                yRotation = 90;
+                carName = "E Car ";
+            }
+
+            _carsExecution[i].name = carName + i;
+            _carsExecution[i].transform.rotation = Quaternion.Euler(new Vector3(0, yRotation, 0));
+
+            /*if (i < 10)
             {
                 _carsExecution[i] = CarpoolManager.Instance.ActivateObject(
                     new Vector3(cars[i].x - _offset + _offsetStreet, cars[i].y, cars[i].z / _ratio)
@@ -26,11 +72,15 @@ public class CarSpawner : MonoBehaviour
             else
             {
                 _carsExecution[i] = CarpoolManager.Instance.ActivateObject(
-                    new Vector3(cars[i].x + _offset - _offsetStreet, cars[i].y, cars[i].z / _ratio)
+                    new Vector3(
+                        cars[i].x + _offset - _offsetStreet,
+                        cars[i].y, 
+                        cars[i].z / _ratio
+                    )
                 );
                 _carsExecution[i].transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
                 _carsExecution[i].name = "N Car " + i;
-            }
+            }*/
         }
         spawned = true;
     }
@@ -43,13 +93,28 @@ public class CarSpawner : MonoBehaviour
             SpawnCars(ref cars);
             return; 
         }
+
+
         for (int i = 0; i < _carsExecution.Length; i++)
         {
-            _carsExecution[i].transform.position = new Vector3(
-                _carsExecution[i].transform.position.x,
-                cars[i].y,
-                cars[i].z / _ratio
-            );
+            int[] currentDirection = cars[i].direction;
+
+            if(currentDirection[0] == 0)
+            {
+                _carsExecution[i].transform.position = new Vector3(
+                    _carsExecution[i].transform.position.x,
+                    cars[i].y,
+                    cars[i].z / _ratio
+                );
+            }
+            else
+            {
+                _carsExecution[i].transform.position = new Vector3(
+                    cars[i].x / _ratio,
+                    cars[i].y,
+                    _carsExecution[i].transform.position.z
+                );
+            }
 
             // A proposal for ease motion
             /*_carsExecution[i].transform.position = Vector3.Lerp(
